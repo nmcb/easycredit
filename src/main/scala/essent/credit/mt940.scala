3c10681468b90558517ca19a27ea10c8ca31aa76
nmcb <marco.borst@splatter.nl>
2016-06-21 09:11:59 +0200

    #1 add open questions

--- src/main/scala/essent/credit/mt940.scala
+++ src/main/scala/essent/credit/mt940.scala
@@ -5,29 +5,38 @@ import java.util.Date
 
 case class MT940Transfer
 (
-  amount:               Amount,
-  statementNumber:      StatementNumber,
-  mutationCode:         MutationCode,
-  accountNumber:        IBAN,
-  contraAccountNumber:  IBAN,
-  transactionTypeID:    TransactionTypeID,
-  date:                 Date,
-  reference:            String,              // reference code or value?
-  mt940DocumentID:      MT940DocumentID,     // move to containing object?
-  transferFncAmount:    Amount,
-  ledgerID:             LedgerID,
-  name:                 String,              // which name?
-  reversal:             Boolean,
-  bic:                  BIC,
-  reversalCode:         ReversalCode,
-  paymentDescription:   String,
-  dontMatch:            Boolean,             // attribute of a transfer?
-  transferDate:         Date,
-  description:          String
+  amount:               Amount,             // TODO Q: Currency ? Rounding Rules ? Resolution ?
+
+  accountNumber:        IBAN,               // TODO Q: Source / Target ?
+  contraAccountNumber:  IBAN,               // TODO Q: Source / Target ?
+
+  name:                 String,             // TODO Q: Of which IBAN identified entity ?
+
+  mutationCode:         MutationCode,       // TODO Q: Meaning ?
+
+  statementNumber:      StatementNumber,    // TODO Q: Sequence of Events (event ordering)
+  date:                 Date,               // TODO Q: Resolution ? Instance/Duration ? (event ordering)
+
+  dontMatch:            Boolean,            // TODO Q: Is this input ? (seems to be a DF marker)
+  transactionTypeID:    TransactionTypeID,  // TODO Q: Reference Match ? (or event ordering)
+  reference:            String,             // TODO Q: Reference Match ? (also some description)
+  mt940DocumentID:      MT940DocumentID,    // TODO Q: Reference Match ? (traceability)
+  paymentDescription:   String,             // TODO Q: Reference Match ? (transfer versus payment)
+
+  transferFncAmount:    Amount,             // TODO Q: Meaning ?
+
+  reversal:             Boolean,            // TODO Q: Dual ?
+  reversalCode:         ReversalCode,       // TODO Q: Dual ?
+
+  ledgerID:             LedgerID,           // TODO Q: ledger :: S -> IBAN -> IBAN -> Ledger ?
+  bic:                  BIC,                // TODO Q: bic :: IBAN -> BIC ?
+
+  transferDate:         Date,               // TODO Q: Resolution ? Instance/Duration ?
+  description:          String              // TODO Q: Reference Match ? (see reference)
 )
 
 case class MT940Document
 (
-  saldoEndDate:         Date,
-  transfers:            Seq[MT940Transfer]
+  saldoEndDate:         Date,               // TODO Q: Resolution ? What is saldo ?
+  transfers:            Seq[MT940Transfer]  // TODO Q: Ordered ? Indexed ?
 )
